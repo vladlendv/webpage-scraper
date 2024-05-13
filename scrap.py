03ad50f58bdca00795cfd8df2b3a2c65de1ed8d7
Vladlen01~ <tubebiznes@gmail.com>
2024-05-13 18:18:27 +0500

    first version, get results from one page

--- scrap.py
+++ scrap.py
@@ -1,2 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+
+url = "https://scrapingclub.com/exercise/list_basic/?page=1"
+response = requests.get(url)
+html_data = BeautifulSoup(response.text, 'lxml')
+product_cards = html_data.find_all("div", class_="w-full rounded border")
+
+test = []
+
+for item in product_cards:
+  item_name = item.find("h4").text.strip('\n')
+  item_price = item.find("h5").text
+  test.append(f"{item_name} {item_price}")
+  
+for i in test:
+  print(i)
