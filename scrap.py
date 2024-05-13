e8dbab86842dd8192b1d0841614788513b902302
Vladlen01~ <tubebiznes@gmail.com>
2024-05-13 19:17:19 +0500

    added delay between requests and added process information

--- scrap.py
+++ scrap.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
-results = []
+from time import sleep
 
 def get_pagination_length():
   url = "https://scrapingclub.com/exercise/list_basic/?page=1"
@@ -10,7 +9,14 @@ def get_pagination_length():
   number_of_pages = html_data.find_all("span", class_="page")
   return len(number_of_pages)
 
-for count in range(1, get_pagination_length()):
+page_count = get_pagination_length()
+results = []
+delay = 1
+
+print("Parsing has started...")
+
+for count in range(1, page_count):
+  sleep(delay)
   url = f"https://scrapingclub.com/exercise/list_basic/?page={count}"
   response = requests.get(url)
   html_data = BeautifulSoup(response.text, 'lxml')
@@ -21,8 +27,9 @@ for count in range(1, get_pagination_length()):
     item_price = item.find("h5").text
     results.append(f"{item_name} {item_price}")
   
+  
 for i in results:
   print(i)
 
-print(f"results length: { len(results) }")
+print(f"Parsing completed successfully, { len(results) } results received. elapsed time { '%.0f' % ((delay * page_count) * 1000) } ms")
 
